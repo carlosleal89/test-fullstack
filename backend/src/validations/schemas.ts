719e3592a7e0f398e18317f966d0ac45d69ee5f9
Carlos Leal <carlos.frleal@gmail.com>
2024-03-14 13:51:18 -0300

    refactor: changed the regular expression to validate the phone

--- backend/src/validations/schemas.ts
+++ backend/src/validations/schemas.ts
@@ -11,8 +11,8 @@ export const createUserValidator = joi.object({
   name: joi.string().regex(/^[a-zA-Z\s]*$/)
     .message('"Nome" não pode conter números ou caracteres especiais').min(4).max(250).required(),
   email: joi.string().email().required(),
-  phone: joi.string().pattern(/^\(\d{2}\)\d{4,5}-\d{4}$/)
-    .message('"Telefone" deve estar no formato (xx)xxxx-xxxx ou (xx)xxxxx-xxxx').required(),
+  phone: joi.string().pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/)
+    .message('"Telefone" deve estar no formato (xx) xxxx-xxxx ou (xx) xxxxx-xxxx').required(),
   status: joi.string().valid(...Object.values(UserStatus)).required(),
 })
 
