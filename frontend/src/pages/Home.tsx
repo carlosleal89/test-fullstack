4025aecc3857409e5ca2d2f5a4a3af38647de218
Carlos Leal <carlos.frleal@gmail.com>
2024-03-14 20:50:08 -0300

    feat: created page home with basic structure

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import api from '../api';
+import Loading from '../components/Loading';
+import { IUsers } from '../interfaces/IUser';
+
 
 function Home() {
+  const [ usersList, setUsersList ] = useState<IUsers[]>([]);
+  
+  useEffect(() => {
+    const getUserList = async () => {
+      const { data } = await api.get('users/');
+      setUsersList(data);
+    };
+    getUserList();
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      {
+        usersList.map((userEl) => (
+          <div key={userEl.id}>
+            <p>{ userEl.name }</p>
+            <p>{ userEl.email }</p>
+            <p>{ userEl.cpf }</p>
+            <p>{ userEl.phone }</p>
+            <p>{ userEl.status }</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
