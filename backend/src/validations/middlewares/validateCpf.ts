209611c0695805ef348cffa6c7e4c0b50f50d4a5
Carlos Leal <carlos.frleal@gmail.com>
2024-03-15 16:10:30 -0300

    refactor: middleware for cpf validation refactored

--- backend/src/validations/middlewares/validateCpf.ts
+++ backend/src/validations/middlewares/validateCpf.ts
@@ -1,28 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function validateCpf(req: Request, res: Response, next: NextFunction): Response | void {
-  let { cpf } = req.body;
+  const { cpf } = req.body;
   
   if (!cpf) {
     return res.status(400).json({
-      message: 'Todos os campos devem ser preenchidos.',
+      message: 'CPF deve ser informado.',
     });
   }
-
-  cpf = cpf.replace(/[^\d]/g, ''); // Remove non numerical chars
-
-  const cpfRegex = /^(\d)\1+$/;
-
-
+  
   if (cpf.length !== 11) {
     return res.status(400).json({
-      message: 'Formato inválido do CPF.',
+      message: 'O CPF deve conter 11 digitos.',
     });
   }
 
-  if (cpfRegex.test(cpf)) {
+  const cpfRegexNotEqualDigits = /^(.)\1*$/;
+
+  if (cpfRegexNotEqualDigits.test(cpf)) {
     return res.status(400).json({
-      message: 'Formato inválido do CPF.',
+      message: 'CPF inválido.',
     });
   }
 
