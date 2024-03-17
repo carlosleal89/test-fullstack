f9a100145e3f9cf2d3b887c4c96a8c6a5b08b8ad
Carlos Leal <carlos.frleal@gmail.com>
2024-03-17 10:34:25 -0300

    feat: inserted a userIcon with the text Painel de clientes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,24 @@ import EditUser from './pages/EditUser';
 import CreateUser from './pages/CreateUser';
 import Header from './components/Header';
 import './App.css';
+import userIcon from './images/user_icon.svg';
 
 function App() {
   return (
     <div>
       <Header />
-      <div className="mt-20 ml-40 mr-40">
-        <h1 className="font-bold text-[2rem]">Painel de Clientes</h1>
-        <div className="mt-10 border-b"></div>
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route exact path='/user/:id' component={ EditUser } />
-          <Route exact path='/new-user' component={ CreateUser } />
-        </Switch>
-      </div>
+        <div className="mt-20 ml-40 mr-40">
+          <div className="flex">
+            <img src={ userIcon } alt="user icon" id="user-icon" className="mr-5 w-10" />
+            <h1 className="font-bold text-[2rem]">Painel de Clientes</h1>
+          </div>
+          <div className="mt-10 border-b"></div>
+          <Switch>
+            <Route exact path='/' component={ Home } />
+            <Route exact path='/user/:id' component={ EditUser } />
+            <Route exact path='/new-user' component={ CreateUser } />
+          </Switch>
+        </div>
     </div>
   );
 }
