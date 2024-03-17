703b0532202fae927e6671fedc6d74d75fd9b256
Carlos Leal <carlos.frleal@gmail.com>
2024-03-17 12:21:46 -0300

    refactor: changed the uol logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/Header.css';
 import uolLogo from '../images/uol-universo-on-line.svg';
+import logo from '../images/UOL.svg';
 
 function Header() {
   return (
     <div className="header_container">
-      <img src={ uolLogo } alt="uol logo" id="header-uol-logo" />
+      <img src={ logo } alt="uol logo" id="header-uol-logo" />
     </div>
   )
 }
